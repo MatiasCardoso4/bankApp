6ff7a655e6c032b3ae10d723b461ddcb04a71293
Matias <cardosomatias1994@gmail.com>
2024-11-24 11:09:48 -0300

    Refactor user info template, enhance logout functionality, and improve CSS styles for better layout

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let user = "";
 const savedUser = localStorage.getItem("user")
   ? JSON.parse(localStorage.getItem("user"))
   : [];
-let userInfo = [];
+
+let userInfo = savedUser || [];
 
 input.addEventListener("input", (e) => {
   user = e.target.value;
@@ -17,18 +18,16 @@ input.addEventListener("input", (e) => {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   checkInput();
-  createUser(userInfo);
+  createUser(user);
   showTable();
-
-  console.log(userInfo);
+  localStorage.setItem("templateContent", clone);
 });
 
 function createUser(user) {
   const ifExists = savedUser.some((user) => user.id === user.id);
   if (!ifExists) {
-    savedUser.push({ userName: user, accountNumber: 1, balance: 0 });
+    savedUser.push({ userName: user, id: 1, balance: 1000 });
   }
-
   localStorage.setItem("user", JSON.stringify(userInfo));
 }
 
@@ -55,35 +54,35 @@ const a = clone.querySelector("a");
 //*************************************\\
 
 function redirect() {
-  formTemplate.addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
+  // formTemplate.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  // });
 
   container.textContent = "";
   container.appendChild(clone);
 }
 
 function showTable() {
-  savedUser.forEach((user) => {
-    userName.textContent = user.userName;
-    accountNumber.textContent = user.accountNumber;
-    userBalance.textContent = user.balance;
-  });
+  if (savedUser) {
+    savedUser.forEach((user) => {
+      userName.textContent = user.userName;
+      accountNumber.textContent = user.id;
+      userBalance.textContent = user.balance;
+    });
+  }
 }
+
+logoutBtn.addEventListener("click", () => {
+  localStorage.removeItem("templateContent");
+  localStorage.removeItem("user");
+
+  a.href = "./index.html";
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   const savedTemplate = localStorage.getItem("templateContent");
-
   if (savedTemplate) {
     redirect();
     showTable();
-  } else {
-    container.append(main);
   }
-  localStorage.setItem("templateContent", clone);
-});
-
-logoutBtn.addEventListener("click", () => {
-  a.href = "./index.html";
-  localStorage.removeItem("templateContent");
 });
